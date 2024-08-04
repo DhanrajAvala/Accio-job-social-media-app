374109624b9ebec65cf3df1c2305efec076475d2
DhanrajAvala <150889983+DhanrajAvala@users.noreply.github.com>
2024-08-04 10:19:25 +0530

    newupdate

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useEffect } from "react";
+import "../App.css";
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPosts, selectPost } from "../reduxContainer/SocialAction.js";
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+  const dispatch = useDispatch();
+  const posts = useSelector(state => state.posts);
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
+  const handleSelectPost = (post) => {
+    dispatch(selectPost(post));
+  };
+
+  return (
+    <div className="home-page">
+      <div className="heading">
+        <h2>Social Media App</h2>
+      </div>
+      <hr />
+      <section className="cards-container">
+        {posts.map((post) => (
+          <div key={post.id} className="card">
+            <div className="img-container">
+              <img src={`https://picsum.photos/200?random=${post.id}`} alt="display-image" />
+            </div>
+            <div className="content">
+              <p>User ID: {post.userId}</p>
+              <p>Title: {post.title.length > 20 ? `${post.title.slice(0, 10)}...` : post.title}</p>
+              <p className="para">
+                Body: {post.body.length > 50 ? `${post.body.slice(0, 40)}...` : post.body} <br />
+                <Link 
+                  to={`/details/${post.id}`} 
+                  onClick={() => handleSelectPost(post)}
+                >
+                  Read More...
+                </Link>
+              </p>
+            </div>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
